acc958d9d5a26d915d947fb784dbb2982a7ea104
Landroid-95 <yang3545535442@gmail.com>
2021-03-05 01:51:33 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+require(`@/plugins/element-ui`)
+require(`@/plugins/clipboard`)
+require(`@/plugins/base64`)
+require(`@/plugins/particles`)
+require(`@/plugins/axios`)
+require(`@/plugins/device`)
+
+import '@/icons' // icon
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')

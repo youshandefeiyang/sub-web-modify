73a78f9afd9f8480c59edfea34ea3cedcc0b5ef5
limr95 <63959400+limr95@users.noreply.github.com>
2021-03-23 22:47:17 +0800

    暗黑模式进一步兼容QQ等浏览器！

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -846,6 +846,7 @@ export default {
   mounted() {
     this.form.clientType = "clash";
     this.getBackendVersion();
+    document.getElementsByTagName('body')[0].setAttribute('class', 'light-mode');
     const lightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)'); 
     const darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)'); 
     if (lightMode && lightMode.matches) { 

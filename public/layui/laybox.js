7a085140232c36e17f6ab9423aab59bde1d75269
肥羊 <yunspeedtest@gmail.com>
2021-07-12 06:03:47 +0800

    Delete laybox.js

--- public/layui/laybox.js
+++
@@ -1,6 +0,0 @@
-layer.ready(function () {
-	   layer.open({
-	    title: '<i class="layui-icon layui-icon-tips"> 温馨提示</i>'
-	    ,content: `<div style="text-align:center">点击加入电报频道：<span><a href="https://t.me/yunspeedtest" style="color:red;font-size:20px;text-decoration:none">机场 ☁️ 测速</a></span></div>`
-	  });     
-	})

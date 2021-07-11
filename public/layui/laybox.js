8206c20ee71f447f78c127658c9bfefea6eb3d80
肥羊 <yunspeedtest@gmail.com>
2021-07-12 06:04:23 +0800

    Add files via upload

---
+++ public/layui/laybox.js
@@ -0,0 +1,6 @@
+layer.ready(function () {
+	   layer.open({
+	    title: '<i class="layui-icon layui-icon-tips"> 温馨提示</i>'
+	    ,content: `<div style="text-align:center">本订阅转换站前端由肥羊二次开发并负责后期功能维护，服务器由高级AIA专线机场牧场物语提供，牧场AIA烤鱼专线月付49即可上车，拥有阿根廷，土耳其，巴西，菲律宾等众多稀有节点，均对接腾讯AIA专线，稳定不过墙，晚高峰轻松秒4K，新人第一次购买可使用优惠码“九折”，感谢大家的支持，牧场物语官网：<span><a href="https://www.mcwy.cloud" style="color:red;font-size:20px;text-decoration:none">点击注册</a></span></div>`
+	  });     
+	})

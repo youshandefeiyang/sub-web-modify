4f6b109774d064b3cb99a49289ac89eb80639d46
肥羊 <yunspeedtest@gmail.com>
2021-10-08 01:41:32 +0800

    Update laybox.js

--- public/layui/laybox.js
+++ public/layui/laybox.js
@@ -1,6 +1,6 @@
 layer.ready(function () {
 	   layer.open({
 	    title: '<i class="layui-icon layui-icon-tips"> 温馨提示</i>'
-	    ,content: `<div style="text-align:center"><strong><span style="font-size:20px">牧场物语官网：</span><span><a href="https://www.mcwy.cloud" style="color:red;font-size:20px;text-decoration:none">点击注册</a></span></strong></br><strong><span style="font-size:20px">奈飞合租网站：</span><span><a href="https://www.ihezu.shop" style="color:red;font-size:20px;text-decoration:none">点击上车</a></span></strong></br>国庆7天临时广告：本站服务器赞助机场-牧场物语，最近搞国庆活动，力度还不错，牧场物语采用最新的奈飞非自制剧解决方案，出口随机更换IP，确保尽可能的每个用户可以用上独立IP，以此来稳定解决奈飞非自制剧的封锁，目前牧场的新加坡，美国，台湾，马来西亚等节点均可做到24H稳定非自制剧观看！国庆七天活动具体为：</br>A组烧鸡烧鸭九折优惠码：aamcwy90</br>B组烤猪烤羊八五折优惠码：bbmcwy85</br>C组烤鱼八折优惠码：ccmcwy80</br></div>`
+	    ,content: `<div style="text-align:center"><strong><span style="font-size:20px">牧场物语官网：</span><span><a href="https://www.mccloud.vip" style="color:red;font-size:20px;text-decoration:none">点击注册</a></span></strong></br><strong><span style="font-size:20px">奈飞合租网站：</span><span><a href="https://www.ihezu.shop" style="color:red;font-size:20px;text-decoration:none">点击上车</a></span></strong></br>国庆7天临时广告：本站服务器赞助机场-牧场物语，最近搞国庆活动，力度还不错，牧场物语采用最新的奈飞非自制剧解决方案，出口随机更换IP，确保尽可能的每个用户可以用上独立IP，以此来稳定解决奈飞非自制剧的封锁，目前牧场的新加坡，美国，台湾，马来西亚等节点均可做到24H稳定非自制剧观看！国庆七天活动具体为：</br>A组烧鸡烧鸭九折优惠码：aamcwy90</br>B组烤猪烤羊八五折优惠码：bbmcwy85</br>C组烤鱼八折优惠码：ccmcwy80</br></div>`
 	  });     
 	})

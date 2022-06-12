80d606a54059de0541df3d5ebbebd2f1eb2f0f86
肥羊 <yunspeedtest@gmail.com>
2022-06-12 17:57:45 +0800

    解决post至后端后中文乱码问题

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -1009,7 +1009,7 @@ export default {
       this.loading = true;
 
       let data = new FormData();
-      data.append("config", this.uploadConfig);
+      data.append("config", encodeURIComponent(this.uploadConfig));
 
       this.$axios
         .post(configUploadBackend, data, {

e0441e8b4a10cec11a738ae930a3505970133563
肥羊 <youshandefeiyang@gmail.com>
2022-06-28 13:55:58 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -100,12 +100,12 @@
                       <el-input v-model="form.rename" placeholder="举例：`a@b``1@2`，|符可用\转义"/>
                     </el-form-item>
                     <el-form-item label="订阅命名:">
-                      <el-input v-model="form.filename" placeholder="返回的订阅文件名"/>
+                      <el-input v-model="form.filename" placeholder="返回的订阅文件名，可以在支持文件名的客户端中显示出来"/>
                     </el-form-item>
                     <el-form-item label="更新间隔:">
                       <el-input v-model="form.interval" placeholder="返用于设置托管配置更新间隔，单位为秒"/>
                     </el-form-item>
-                    <el-form-item label="远程设备ID:">
+                    <el-form-item label="远程设备:">
                       <el-input v-model="form.devid" placeholder="用于设置QuantumultX的远程设备ID"/>
                     </el-form-item>
                     <el-form-item class="eldiy" label-width="0px">

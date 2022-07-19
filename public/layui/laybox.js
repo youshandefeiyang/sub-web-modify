89d08ed25f85126e3c312ab2ab0648246cf1d99c
肥羊 <youshandefeiyang@gmail.com>
2022-07-19 23:36:26 +0800

    Delete laybox.js

--- public/layui/laybox.js
+++
@@ -1,6 +0,0 @@
-layer.ready(function () {
-	   layer.open({
-	    title: '<i class="layui-icon layui-icon-tips"> 临时广告</i>'
-	    ,content: `<div style="text-align:center"><strong><span style="font-size:20px">牧场物语官网：</span><span><a href="https://www.mccloud.vip" style="color:red;font-size:20px;text-decoration:none">点击注册</a></span></strong></br><strong><span style="font-size:20px">奈飞合租网站：</span><span><a href="https://www.ihezu.shop" style="color:red;font-size:20px;text-decoration:none">点击上车</a></span></strong></br>本站服务器赞助机场-牧场物语，是一家拥有CN2+BGP+IEPL内网专线的高端机场，适合各个价位要求的用户，牧场物语采用最新的奈飞非自制剧解决方案，出口随机更换IP，确保尽可能的每个用户可以用上独立IP，以此来稳定解决奈飞非自制剧的封锁，并推出7*24小时奈飞非自制剧节点自动检测系统，用户再也不用自己手动一个个的乱试节点了，目前牧场的新加坡，台湾等节区域点均可做到24H稳定非自制剧观看！</br></div>`
-	  });     
-	})

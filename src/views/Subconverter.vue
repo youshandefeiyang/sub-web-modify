c753fdaf1fd5463fb4eb23a715fceb12180d8972
肥羊 <youshandefeiyang@gmail.com>
2023-06-30 14:05:33 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -424,7 +424,7 @@ const advancedVideo = process.env.VUE_APP_ADVANCED_VIDEO
 const tgBotLink = process.env.VUE_APP_BOT_LINK
 const yglink = process.env.VUE_APP_YOUTUBE_LINK
 const bzlink = process.env.VUE_APP_BILIBILI_LINK
-const downld = process.env.VUE_APP_PROXYTOOLS
+const downld = 'http://' + window.location.hostname + '/download.html'
 export default {
   data() {
     return {

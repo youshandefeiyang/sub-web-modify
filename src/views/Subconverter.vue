a6eafb09b15a0e65ca201149fcca87b1bb040ad5
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2024-02-18 09:03:13 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -6,7 +6,7 @@
           <div slot="header">
             <svg-icon class="gayhub" icon-class="github" style="float:left" @click="goToProject"/>
             <svg-icon class="dianbao" icon-class="telegram" style="float:left;margin-left: 10px"
-                      @click="gotoTgChannel"/>
+                      <a href="tg://user?id=5480911121"></a>/>
             <svg-icon class="bilibili" icon-class="bilibili" style="float:right;margin-left:10px"
                       @click="gotoBiliBili"/>
             <svg-icon class="youguan" icon-class="youtube" style="float:right;margin-left:10px" @click="gotoYouTuBe"/>

e816d728eaedcdff31764ca5b0a8f7cc1dec40db
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2024-02-18 10:27:14 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -462,7 +462,7 @@ export default {
         },
         shortTypes: {
           "v1.mk": "https://v1.mk/short",
-          "大象短链": "https://s.dxdyzh.tk",
+          "大象短链a": "https://duanlian.scyucitizen1.workers.dev/citizenscyu",
           "d1.mk": "https://d1.mk/short",
           "dlj.tf": "https://dlj.tf/short",
           "suo.yt": "https://suo.yt/short",
